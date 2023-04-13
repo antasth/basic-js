0e5412c534813a6c5d2ff1f02a691533df906cd8
antasth <antasth@mail.ru>
2023-04-14 00:53:38 +0300

    feat: complete sum-digits task

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require('../extensions/index.js')
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +23,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+matrix = [
+  [true, false, false],
+  [false, true, false],
+  [false, false, false],
+]
+function minesweeper(matrix) {
+  matrix.forEach((item) => {
+    for (let i = 0; i < item.length; i++) {
+      item[i] === true ? (item[i] = 1) : (item[i] = 0)
+    }
+  })
+  matrix.forEach(item => {
+    for(let i =0; i< item.length; i++) {
+
+    }
+  })
+  console.log(matrix)
 }
+minesweeper(matrix)
 
 module.exports = {
-  minesweeper
-};
+  minesweeper,
+}
