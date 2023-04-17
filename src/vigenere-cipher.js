082df55e058c9d1023312db0a7ec212580f86c93
antasth <antasth@mail.ru>
2023-04-18 01:02:48 +0300

    feat: complete decrypt method in vinegere-cypher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -21,10 +21,12 @@ const { NotImplementedError } = require('../extensions/index.js')
  */
 
 class VigenereCipheringMachine {
-  constructor() {
+  constructor(type) {
     this.alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+    this.type = (type = false) ? 'reverse' : 'direct'
   }
   encrypt(string, key) {
+    if (!string || !key) throw new Error('Incorrect arguments!')
     let str = string.toLowerCase().replace(/ /g, '')
     let stringIndexes = []
     let keyIndexes = []
@@ -67,16 +69,75 @@ class VigenereCipheringMachine {
         cryptedString.splice(i, 0, ' ')
       }
     }
-    return cryptedString.join('').toUpperCase()
+    if (this.type === 'direct') {
+      return cryptedString.join('').toUpperCase()
+    } else {
+      return cryptedString.reverse().join('').toUpperCase()
+    }
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented')
-    // remove line with error and write your code here
+  decrypt(string, key) {
+    if (!string || !key) throw new Error('Incorrect arguments!')
+    let str = string.toLowerCase().replace(/ /g, '')
+    let stringIndexes = []
+    let keyIndexes = []
+    let cipherIndexes = []
+    let deCryptedString = []
+
+    str.split('').forEach((element) => {
+      this.alphabet.includes(element)
+        ? stringIndexes.push(this.alphabet.indexOf(element))
+        : stringIndexes.push(element)
+    })
+
+    key
+      .toLowerCase()
+      .split('')
+      .forEach((element) => {
+        this.alphabet.includes(element)
+          ? keyIndexes.push(this.alphabet.indexOf(element))
+          : keyIndexes.push(element)
+      })
+
+    while (keyIndexes.length < stringIndexes.length) {
+      keyIndexes = [...keyIndexes, ...keyIndexes]
+    }
+    keyIndexes = keyIndexes.splice(0, stringIndexes.length)
+
+    for (let i = 0; i < stringIndexes.length; i++) {
+      if (typeof stringIndexes[i] === 'number') {
+        let num = stringIndexes[i] - (Number(keyIndexes[i]) % 26)
+        if (num > 0) {
+          cipherIndexes.push(num)
+        } else {
+          num = (num + 26) % 26
+          cipherIndexes.push(num)
+        }
+      } else {
+        cipherIndexes.push(stringIndexes[i])
+      }
+    }
+
+    for (let i = 0; i < cipherIndexes.length; i++) {
+      typeof cipherIndexes[i] === 'number'
+        ? deCryptedString.push(this.alphabet[cipherIndexes[i]])
+        : deCryptedString.push(cipherIndexes[i])
+    }
+
+    let nonCryptedStr = string.split('')
+    for (let i = 0; i < nonCryptedStr.length; i++) {
+      if (nonCryptedStr[i] === ' ') {
+        deCryptedString.splice(i, 0, ' ')
+      }
+    }
+
+    if (this.type === 'direct') {
+      return deCryptedString.join('').toUpperCase()
+    } else {
+      return deCryptedString.reverse().join('').toUpperCase()
+    }
   }
 }
 
-const directMachine = new VigenereCipheringMachine()
-directMachine.encrypt('Samelengthkey', 'Samelengthkey')
 
 module.exports = {
   VigenereCipheringMachine,
