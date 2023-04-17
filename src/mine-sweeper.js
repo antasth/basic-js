85500ee6fa85c3700beb46c94610e29a57ad1c09
antasth <antasth@mail.ru>
2023-04-18 00:16:59 +0300

    feat: complete encrypt in vigenere-cipher task

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -24,27 +24,56 @@ const { NotImplementedError } = require('../extensions/index.js')
  * ]
  */
 
-// matrix = [
-//   [true, false, false],
-//   [false, true, false],
-//   [false, false, false],
-// ]
-function minesweeper(/*matrix */) {
-  throw new NotImplementedError('Not implemented');
+matrix = [
+  [true, false, false],
+  [false, true, false],
+  [false, false, false],
+]
 
-  // matrix.forEach((item) => {
-  //   for (let i = 0; i < item.length; i++) {
-  //     item[i] === true ? (item[i] = 1) : (item[i] = 0)
-  //   }
-  // })
-  // matrix.forEach(item => {
-  //   for(let i =0; i< item.length; i++) {
+function minesweeper(matrix) {
+  throw new NotImplementedError('Not implemented')
+  // remove line with error and write your code here
+  // let width = matrix[0].length
+  // let heigth = matrix.length
+  // let res = []
 
+  // // for (i = 0; i < width; i++) {
+  // //   res[i] = new Array()
+  // //   for (j = 0; j < heigth; j++) {
+  // //     res[i][j] = 0
+  // //   }
+  // // }
+
+  // for (let i = 0; i < width; i++) {
+  //   for (let j = 0; j < heigth; j++) {
+  //     // console.log(matrix[i][j])
+  //     // adjacentBombs(i, j)
+  //     console.log(adjacentBombs(i, j))
   //   }
-  // })
-  // console.log(matrix)
+  // }
+
+  // function adjacentBombs(row, col) {
+  //   let i, j, count
+  //   count = 0
+
+  //   for (i = row - 1; i < row + 1; i++) {
+  //     for (j = col - 1; j < col + 1; j++) {
+  //       // if (i > 0 && j > 0 && matrix[i][j]) {
+  //       if (i > 0 && j > 0 && matrix[i][j] !== undefined) {
+  //         //   if (i == row && j == col) continue
+  //         //   // console.log(matrix[i][j])
+  //         //   count++
+  //         // }
+  //         console.log(matrix[i][j])
+  //       }
+  //     }
+  //   }
+  //   // return count
+  // }
+
+  // return res
 }
-// minesweeper(matrix)
+// console.log(minesweeper(matrix))
 
 module.exports = {
   minesweeper,
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,53 +1,85 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require('../extensions/index.js')
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
+
 class VigenereCipheringMachine {
- 
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-    const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  constructor() {
+    this.alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+  }
+  encrypt(string, key) {
+    let str = string.toLowerCase().replace(/ /g, '')
+    let stringIndexes = []
+    let keyIndexes = []
+    let cipherIndexes = []
+    let cryptedString = []
+    str.split('').forEach((element) => {
+      this.alphabet.includes(element)
+        ? stringIndexes.push(this.alphabet.indexOf(element))
+        : stringIndexes.push(element)
+    })
+    key
+      .toLowerCase()
+      .split('')
+      .forEach((element) => {
+        this.alphabet.includes(element)
+          ? keyIndexes.push(this.alphabet.indexOf(element))
+          : keyIndexes.push(element)
+      })
 
+    while (keyIndexes.length < stringIndexes.length) {
+      keyIndexes = [...keyIndexes, ...keyIndexes]
+    }
+    keyIndexes = keyIndexes.splice(0, stringIndexes.length)
 
+    for (let i = 0; i < stringIndexes.length; i++) {
+      typeof stringIndexes[i] === 'number'
+        ? cipherIndexes.push((stringIndexes[i] + Number(keyIndexes[i])) % 26)
+        : cipherIndexes.push(stringIndexes[i])
+    }
+
+    for (let i = 0; i < cipherIndexes.length; i++) {
+      typeof cipherIndexes[i] === 'number'
+        ? cryptedString.push(this.alphabet[cipherIndexes[i]])
+        : cryptedString.push(cipherIndexes[i])
+    }
+
+    let nonCryptedStr = string.split('')
+    for (let i = 0; i < nonCryptedStr.length; i++) {
+      if (nonCryptedStr[i] === ' ') {
+        cryptedString.splice(i, 0, ' ')
+      }
+    }
+    return cryptedString.join('').toUpperCase()
   }
   decrypt() {
-    throw new NotImplementedError('Not implemented');
+    throw new NotImplementedError('Not implemented')
     // remove line with error and write your code here
   }
 }
 
+const directMachine = new VigenereCipheringMachine()
+directMachine.encrypt('Samelengthkey', 'Samelengthkey')
+
 module.exports = {
-  VigenereCipheringMachine
-};
-
-function matrixArray(rows,columns){
-  var arr = new Array();
-  for(var i=0; i<rows; i++){
-    arr[i] = new Array();
-    for(var j=0; j<columns; j++){
-      arr[i][j] = i+j+1;//вместо i+j+1 пишем любой наполнитель. В простейшем случае - null
-    }
-  }
-  return arr;
+  VigenereCipheringMachine,
 }
-var myMatrix = matrixArray(3,3);
 
-// console.log(myMatrix);
+console.log(typeof 0)
